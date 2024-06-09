bb6e64a595ae2fafe29a19330bd749790b24938f
unchainedraggedy <112824595+unchainedraggedy@users.noreply.github.com>
2024-06-09 15:27:41 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+    let d = Math.pow(b, 2) - 4 * a * c;
+    let x1, x2;
+    if (d < 0){
+        return arr;
+    } else if (d === 0){
+        x1 = -b / (2 * a);
+        arr.push(x1);
+        return arr;
+    } else if (d > 0){
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        arr.push(x1);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        arr.push(x2);
+        return arr;
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+    let loanBody = amount - contribution;
+    let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+    let totalPayment = monthlyPayment * countMonths;
+    totalPayment = totalPayment.toFixed(2); 
+    return parseFloat(totalPayment);
 }
