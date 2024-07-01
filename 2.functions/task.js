11b33d5293bf0e80953ca65516201fdc9883279a
unchainedraggedy <112824595+unchainedraggedy@users.noreply.github.com>
2024-07-01 14:10:50 +0300

    first task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,20 @@
 function getArrayParams(...arr) {
+    let min, max, sum;
+    min = max = sum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        min = Math.min(arr[0], arr[i]);
+        max = Math.max(arr[0], arr[i]);
+        sum += arr[i];
+    }
+    sum = sum / arr.length;
 
-  return { min: min, max: max, avg: avg };
+    let obj = {};
+    obj.min = min;
+    obj.max = max;
+    obj.avg = sum.toFixed(2);
+
+    return obj;
 }
-
 function summElementsWorker(...arr) {
 
 }
