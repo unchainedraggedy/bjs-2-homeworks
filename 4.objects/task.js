78f31322504f7aeb999c00eff839b637763d3ef6
Ash <redcoolike@gmail.com>
2024-07-04 13:44:04 +0300

    4 tests

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender; 
+    this.age = age;
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks) {
+    if (this.hasOwnProperty('excluded')) {
+        return; 
+      }
+      
+      if (!this.marks) {
+        this.marks = []; 
+      }
+      
+      this.marks.push(...marks);
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    let average = 0;
+    if (this.marks && this.marks.length > 0) {
+      let sum = this.marks.reduce((acc, item) => acc + item, 0);
+      average = sum / this.marks.length;
+    }
+    return average;
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    this.marks = undefined;
+    this.excluded = reason;
+  }
