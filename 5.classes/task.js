21003bd11c3dd32520089fbbd7a1b9aeeb2dc00b
Ash <redcoolike@gmail.com>
2024-07-08 12:11:02 +0300

    beautified task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,95 +1,96 @@
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this._state = 100;
-        this.type = null;
-    }
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this._state = 100;
+		this.type = null;
+	}
 
-    fix() {
-        this.state = this._state * 1.5;
-    }
+	fix() {
+		this.state = this._state * 1.5;
+	}
 
-    set state(newState) {
-        if (newState < 0) {
-            this._state = 0;
-        } else if (newState > 100) {
-            this._state = 100;
-        } else {
-            this._state = newState;
-        }
-    }
+	set state(newState) {
+		if (newState < 0) {
+			this._state = 0;
+		} else if (newState > 100) {
+			this._state = 100;
+		} else {
+			this._state = newState;
+		}
+	}
 
-    get state() {
-        return this._state;
-    }
+	get state() {
+		return this._state;
+	}
 }
 
 class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.type = 'magazine';
-    }
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = 'magazine';
+	}
 }
 
 class Book extends PrintEditionItem {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
-        this.type = 'book';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = 'book';
+	}
 }
 
 class NovelBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount);
-        this.type = 'novel';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'novel';
+	}
 }
 
 class FantasticBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount);
-        this.type = 'fantastic';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'fantastic';
+	}
 }
 
 class DetectiveBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount);
-        this.type = 'detective';
-    }
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'detective';
+	}
 }
 
 class Library {
-    constructor(name) {
-        this.name = name;
-        this.books = [];
-    }
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
 
-    addBook(book) {
-        if (book.state > 30) {
-            this.books.push(book);
-        }
-    }
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
 
-    findBookBy(type, value) {
-        for (const book of this.books) {
-            if (book[type] === value) {
-                return book;
-            }
-        }
-        return null;
-    }
+	findBookBy(type, value) {
+		let bookFound = null;
+		for (const book of this.books) {
+			if (book[type] === value) {
+				bookFound = book;
+			}
+		}
+		return bookFound;
+	}
 
-    giveBookByName(bookName) {
-        const bookIndex = this.books.findIndex(book => book.name === bookName);
-        if (bookIndex !== -1) {
-            const book = this.books[bookIndex];
-            this.books.splice(bookIndex, 1);
-            return book;
-        }
-        return null;
-    }
+	giveBookByName(bookName) {
+		let bookSpliced = null;
+		const bookIndex = this.books.findIndex(book => book.name === bookName);
+		if (bookIndex !== -1) {
+			bookSpliced = this.books[bookIndex];
+			this.books.splice(bookIndex, 1);
+		}
+		return bookSpliced;
+	}
 }
